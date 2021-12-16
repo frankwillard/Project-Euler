ad8b6ae775d371d72ede6941b8d1d00ec1c2107c
Frankie Willard <69867065+frankwillard@users.noreply.github.com>
2021-12-16 14:11:33 -0500

    Update EulerProject97.py

--- EulerProject97.py
+++ EulerProject97.py
@@ -1,3 +1,6 @@
+import time 
+start = time.time()
 largenum = 28433* 2 ** 7830457+1
 tendigmod = 10 ** 10
-print(largenum % tendigmod)
+elapsed = (time.time() - start)
+print ("found %s in %s seconds" % (largenum % tendigmod,elapsed))
