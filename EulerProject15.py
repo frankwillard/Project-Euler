faaeee3a3db64034db4b5a920dcb18390b75caea
Frankie Willard <69867065+frankwillard@users.noreply.github.com>
2021-12-16 13:58:35 -0500

    Update EulerProject15.py

--- EulerProject15.py
+++ EulerProject15.py
@@ -1,5 +1,8 @@
+import time 
+start = time.time()
 import math
 # Cheater method
 size = 20
 paths = math.factorial(size*2)/(math.factorial(size)* math.factorial(size))
-print(paths)
+elapsed = (time.time() - start)
+print ("found %s in %s seconds" % (paths,elapsed))
